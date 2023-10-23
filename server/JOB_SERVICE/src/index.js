4098d8c7924c6e55e40e6adda07f3b9ada9f6050
Abhinav Kumar <abhinavkumar.ak9@gmail.com>
2023-10-24 01:53:07 +0530

    completed job service

--- server/JOB_SERVICE/src/index.js
+++ server/JOB_SERVICE/src/index.js
@@ -1,27 +1,25 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-// const swaggerUi = require("swagger-ui-express");
-// const swaggerSpec = require("./config/swaggerConfig"); 
-const { PORT } = require("./config/serverConfig");
-// const ApiRoutes = require('./routes/index');
-const db = require("./models/index");
-// const {User,Role} = require("./models/index");
-const setupServer = async() => {
+const express = require('express');
+const bodyParser = require('body-parser'); 
+const { PORT } = require('./config/serverConfig');
+const ApiRoutes = require('./routes/index');
+// const db = require('./models/index')
 
+const setupServer = async () => {
+    
+    //Create express object
     const app = express();
+
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
-    // app.use("/authservice/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-    // app.use('/authservice/api',ApiRoutes);
-    app.listen(PORT ,async()=>{
-        console.log(`Server started at port ${PORT}`);
-        // await db.sequelize.sync({alter:true});
-        // const u1 = await User.findByPk(24);
-        // const r1 = await Role.findByPk(1);
-        // u1.addRole(r1);
-    });
 
+    // If any incoming request has /api,it's gonna map to ApiRoutes
+    app.use('/jobservice/api', ApiRoutes);
+
+    app.listen(PORT,() => {
+        console.log(`Server started at ${PORT}`);
+        // Uncomment below line when you make any changes in associations and all
+        // db.sequelize.sync({alter:true});
+    });
 }
 
-setupServer();
-
+setupServer(); 
