41d55b0d4a567667c93953219d6b10c8a53dab74
Abhinav Kumar <abhinavkumar.ak9@gmail.com>
2023-10-24 05:37:32 +0530

    created routes

--- server/APPLICATION_SERVICE/src/routes/v1/index.js
+++ server/APPLICATION_SERVICE/src/routes/v1/index.js
@@ -1,17 +1,11 @@
 const express = require('express');
-// const { CompanyController, JobController} = require('../../controllers/index');
-// const { JobMiddleware } = require("../../middlewares/index");
+const { ApplicationController} = require('../../controllers/index');
+const { ApplicationMiddleware } = require("../../middlewares/index");
 const router = express.Router();
 
-
-// router.post('/companies',JobMiddleware.authorizeAdmin, CompanyController.create); //admin
-// router.get('/companies/:id', CompanyController.get);
-// router.patch('/companies/:id',JobMiddleware.authorizeAdmin, CompanyController.update); //admin
-// router.delete('/companies/:id',JobMiddleware.authorizeAdmin, CompanyController.destroy); //admin
-// router.get('/companies',CompanyController.getAll);
-
-
-
-
+router.post('/applications',ApplicationMiddleware.authenticateUser, ApplicationController.create); //Authenticated user
+router.get('/applications/:id',ApplicationMiddleware.authorizeAdmin, ApplicationController.get); //Admin
+router.delete('/applications/:id',ApplicationMiddleware.authorizeAdmin, ApplicationController.destroy); //admin
+router.get('/applications', ApplicationMiddleware.authorizeAdmin,ApplicationController.getAll);//admin
 
 module.exports = router;
